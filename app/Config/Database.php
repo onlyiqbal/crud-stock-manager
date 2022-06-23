eef5f1c9b62f0686d85121e11baf60df803ab55a
iqbal <iqbalmenggala29@gmail.com>
2022-06-23 19:40:43 +0700

    add file database

---
+++ app/Config/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Iqbal\StockManager\Config;
+
+use PDO;
+
+class Database
+{
+     private static ?PDO $pdo = null;
+
+     public static function getConnection(string $env = "test"): PDO
+     {
+          if (self::$pdo == null) {
+               require_once __DIR__ . "/../../config/database.php";
+               $config = getDatabaseConfig();
+               self::$pdo = new PDO(
+                    $config['database'][$env]['url'],
+                    $config['database'][$env]['username'],
+                    $config['database'][$env]['password']
+               );
+          }
+
+          return self::$pdo;
+     }
+
+     public static function beginTransaction()
+     {
+          self::$pdo->beginTransaction();
+     }
+
+     public function commitTransaction()
+     {
+          self::$pdo->commit();
+     }
+
+     public static function rollBackTrasaction()
+     {
+          self::$pdo->rollBack();
+     }
+}
---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+function getDatabaseConfig(): array
+{
+     return [
+          "database" => [
+               "test" => [
+                    "url" => "mysql:localhost=3306;dbname=db_stock_test",
+                    "username" => "root",
+                    "password" => ""
+               ],
+               "prod" => [
+                    "url" => "mysql:localhost=3306;dbname=db_stock_tes",
+                    "username" => "root",
+                    "password" => ""
+               ]
+          ]
+     ];
+}
